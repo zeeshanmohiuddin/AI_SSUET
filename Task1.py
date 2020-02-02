666b9930ea2e77d668cf90a275ec24dff3ba1037
‘zeeshanmohiuddin’ <zeeshanmohiuddin27@gmail.com>
2020-02-02 21:51:07 +0500

    First Task

---
+++ Task1.py
@@ -0,0 +1,10 @@
+First_Name = 'Zeeshan'
+Last_Name = 'Mohiuddin'
+Age = 23
+Gender = 'Male'
+Course = 'Artificial Intelligence'
+Course_Instructor  = 'Sir Umair'
+
+
+print('Name: ',First_Name + '\nLast Name: ',Last_Name + '\nGender: ',Gender +'\nAge: ',str(Age) + '\nCourse: ',Course + '\nInstructor: ',Course_Instructor)
+
